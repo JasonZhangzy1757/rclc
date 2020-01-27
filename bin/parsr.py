7b52372b55b962228ee6bb905aa279b46e78b1a4
Zheyuan Zhang <56742253+JasonZhangzy1757@users.noreply.github.com>
2020-01-27 15:22:58 -0500

    Add files via upload

--- bin/parsr.py
+++ bin/parsr.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # coding: utf-8
 
 from parsr_client import ParserClient
@@ -13,18 +12,22 @@ def Convert(current_path):
     config_path = current_path + 'bin/sampleConfig.json'
     for lists in os.listdir(pdf_Dir): 
         pdf_path = pdf_Dir + '/' + lists
+        # Send Document for processing 
         job = parsr.send_document(
             file = pdf_path,
             config = config_path,
             wait_till_finished=True,
             save_request_id=True,
         ) 
+        
+        # Get the full JSON output. 
         json_Dir = RootDir + '/json'
         mkdir(json_Dir)
         json_path = json_Dir + '/' + lists + '.json'
         with open(json_path, 'w', encoding="utf-8") as outfile:
             json.dump(parsr.get_json(), outfile, indent=2, ensure_ascii=False)
             
+        # Get the raw text output.    
         text_Dir = RootDir  + '/text'
         mkdir(text_Dir)
         text_path =  text_Dir + '/' + lists + '.txt'
@@ -33,7 +36,7 @@ def Convert(current_path):
 
 
 def mkdir(path):
-
+    # Check if the folder is already exist, if not, create a new one.
     folder = os.path.exists(path)
  
     if not folder:                   
@@ -43,6 +46,7 @@ def mkdir(path):
         
 if __name__ == "__main__":
     
+    # Initialize the client object.
     parsr = ParserClient('localhost:3001')
     
     current_path = os.path.dirname(os.path.dirname(__file__))
